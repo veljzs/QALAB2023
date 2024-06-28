2112ecb94a42f63cce66af0557ee7c8893acfbfa
Verónica Jiménez <128405596+veljzs@users.noreply.github.com>
2024-06-27 21:13:06 -0500

    Arreglo de carpeta y añadidura de linea 24

---
+++ AppArrays/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Declaración de variables
+        int edad;
+        int diasSemana;
+
+        //Inicializacion
+        edad = 30;
+
+        //Asignacion o cambio de valor
+        edad = 20;
+
+        //Declaracion + Inicializacion
+        int dias = 30;
+        System.out.println( "Edad : " + edad);
+
+
+
+
+        String user = config[XPROD][CUSER];
+
+    }
+}
